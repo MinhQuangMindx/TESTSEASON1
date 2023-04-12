39854028f69483683d3293df70f4752813ff3f0e
MinhQuangMindx <vincentequang97@gmail.com>
2023-04-12 21:53:26 +0700

    Create app.js

---
+++ BAITAP1/app.js
@@ -0,0 +1,16 @@
+var a, b;
+a = prompt("Nhập vào số a: ");
+b = prompt("Nhập vào số b");
+
+a = parseInt(a);
+b = parseInt(b);
+
+if (a>b) {
+    alert("Hãy nhập lại số cho a < b");
+} else {
+    
+}
+
+for (i=0; i<b; i++) {
+    
+}
